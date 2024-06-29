f4ec79196b686d1c09eb994828683b9b4be5491e
haritha2001116 <78479429+haritha2001116@users.noreply.github.com>
2024-06-29 10:29:18 +0530

    initial project setup

---
+++ src/test/java/com/foodBudy_v2/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodBudy_v2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
